a925ab1f73f5222712aebb2db662a8ad60305d78
LucasH2o <lucass20586@gmail.com>
2020-09-27 20:14:38 -0300

    Java

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,8 +1,12 @@
-package com.codewithmosh;
-
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(final String[] args) {
+        // final int myAge = 30;
+        // final int herAge = myAge;
+        // System.out.println(herAge);
+        
+        byte age = 30;
+//        int viewsCount = 3_123_456_789;
+          long viewsCount = 3_123_456_789L;
     }
 }
