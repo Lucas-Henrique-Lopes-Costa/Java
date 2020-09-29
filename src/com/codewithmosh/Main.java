793d8cf865359989d8e0951154223fffc612b61f
Lucas Henrique Lopes Costa <60316602+Lucas-Henrique-Lopes-Costa@users.noreply.github.com>
2020-09-28 23:28:51 -0300

    Update

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,3 +1,5 @@
+package com.codewithmosh;
+
 public class Main {
 
     public static void main(final String[] args) {
@@ -6,7 +8,15 @@ public class Main {
         // System.out.println(herAge);
         
         byte age = 30;
-//        int viewsCount = 3_123_456_789;
-          long viewsCount = 3_123_456_789L;
+        // int viewsCount = 3_123_456_789;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+        System.out.println(age);
+        System.out.println(viewsCount);
+        System.out.println(price);
+        System.out.println(letter);
+        System.out.println(isEligible);
     }
 }
