cb057c2693c7992c9cb55da1a08ed86c3a42dd9a
Lucas Henrique Lopes Costa <60316602+Lucas-Henrique-Lopes-Costa@users.noreply.github.com>
2020-10-04 00:56:13 -0300

    String

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -3,10 +3,47 @@ package com.codewithmosh;
 public class Main {
 
     public static void main(String[] args) {
+        String traco = "--------------------------------------------------------------------------------------------------------------------------";
         
+        
+        String message = new String("Hello World");
+        String messageSimple = "   Hello World    " + "!!";
+        message.endsWith("!!");
+        
+        System.out.println(message);
+        System.out.println(messageSimple);
 
-        System.out.println("--------------------------------------------------------------------------------------------------------------------------");
+    System.out.println(traco);
         
-        System.out.println("Hello World");
+        System.out.println(message.endsWith("!!")); // tem no final da frase ? => Bolean(false/true)
+        System.out.println(messageSimple.endsWith("!!"));
+
+    System.out.println(traco);
+
+        System.out.println(message.length()); // conta quantas palaras
+
+    System.out.println(traco);
+        
+        System.out.println(message.indexOf("e"));
+        System.out.println(message.indexOf("Sky"));
+
+    System.out.println(traco);
+
+        System.out.println(messageSimple.replace("!!", "??")); //Isso são "promotes"
+
+    System.out.println(traco);
+
+        System.out.println(messageSimple.toUpperCase());
+        System.out.println(messageSimple.toLowerCase());
+
+    System.out.println(traco);
+
+        System.out.println(message.trim());
+
+    System.out.println(traco);
+
+        // Mas, o Java não altera a String principal:
+        System.out.println(message);
+
     }
 }
