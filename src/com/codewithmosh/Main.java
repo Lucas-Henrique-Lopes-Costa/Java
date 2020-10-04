98dbf3c315b0301e4c1858cde60288b462e30fd9
Lucas Henrique Lopes Costa <60316602+Lucas-Henrique-Lopes-Costa@users.noreply.github.com>
2020-10-04 02:07:55 -0300

    Convertendo Unidades

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -4,9 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
         String traco = "--------------------------------------------------------------------------------------------------------------------------";
-
         System.out.println(traco);
         
-        System.out.println("Hello World");
+        // Inplicit CASTING
+        // byte > short > int > long > float > double
+
+        // ---- Convertendo unidades
+
+        // double x = 1.1;
+        // double y = (int)x + 2; // converter para int
+        // System.out.println(y);
+
+        // ------------------------------------------------------
+
+        // String x = "1";
+        // Integer.parseInt(x); // passa para int
+        // System.out.println(x);
+
+        // int y = Integer.parseInt(x) + 2;
+        // System.out.println(y);
+
+
+        String x = "1.1";
+        double y = Double.parseDouble(x) + 2; // passa para Double
+        System.out.println(y);
+
     }
 }
