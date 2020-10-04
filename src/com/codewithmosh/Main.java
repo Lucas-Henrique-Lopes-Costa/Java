9213bd2a3e04d0ceb4fd4599a60b16f357323ca4
Lucas Henrique Lopes Costa <60316602+Lucas-Henrique-Lopes-Costa@users.noreply.github.com>
2020-10-04 01:08:25 -0300

    Editando Strings

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -5,8 +5,27 @@ public class Main {
     public static void main(String[] args) {
         String traco = "--------------------------------------------------------------------------------------------------------------------------";
 
+        String message = "Hello \"Marsh\"";
+
+        System.out.println(message);
+
+    System.out.println(traco);
+
+        String way = "c:\\WIndows\\...";
+
+        System.out.println(way);
+
+    System.out.println(traco);
+
+        String JumpLine = "c:\nWIndows\\...";
+        System.out.println(JumpLine);
+
         System.out.println(traco);
+
+        String space = "c:\tWIndows\\...";
+        System.out.println(space);
+
+    System.out.println(traco);
         
-        System.out.println("Hello World");
     }
 }
