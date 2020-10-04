edb2f7c5ff8ae648fe2fca30964cfcbf248e0bef
Lucas Henrique Lopes Costa <60316602+Lucas-Henrique-Lopes-Costa@users.noreply.github.com>
2020-10-04 12:45:13 -0300

    Melhoria

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -4,9 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         String traco = "--------------------------------------------------------------------------------------------------------------------------";
-
         System.out.println(traco);
         
+        
+
         System.out.println("Hello World");
     }
 }
