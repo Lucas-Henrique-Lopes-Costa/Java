57bc01784f7f26ca5c5dc3afc2d9ea2b57b817d6
Lucas Henrique Lopes Costa <60316602+Lucas-Henrique-Lopes-Costa@users.noreply.github.com>
2020-10-11 13:37:17 -0300

    Imput feitos

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -8,18 +8,25 @@ public class Main {
         String traco = "--------------------------------------------------------------------------------------------------------------------------";
         System.out.println(traco);
 
-        // duração em meses = n
+        // valor emprestado = P        
         // taxa de juros = i
-        // valor emprestado = P
+        // duração em meses = n
+
         // pagamento mensal = M
 
-        System.out.print("Valor do empréstimo: ");
-        Scanner scannerP = new Scanner(System.in);
-        Byte I = scannerP.nextByte();
+        // System.out.print("Valor do empréstimo: ");
+        // Scanner scannerP = new Scanner(System.in);
+        // Byte P = scannerP.nextByte();
 
-        System.out.print(scannerP);
+        // System.out.print("Qual a taxa de juros ANUAIS: ");
+        // Scanner scannerI = new Scanner(System.in);
+        // float I = scannerI.nextFloat() / 100; // deve-se inserir com ","
 
-        // System.out.print("Número de anos: ");
+        System.out.print("Número de anos: ");
+        Scanner scannerN = new Scanner(System.in);
+        Byte N = scannerN.nextByte();
+
+        
 
 
         // System.out.print("Juros anuais: ");
