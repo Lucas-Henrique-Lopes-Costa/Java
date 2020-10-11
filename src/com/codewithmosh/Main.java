29015763a863d22000cc159d5b9caef0e9de9b44
Lucas Henrique Lopes Costa <60316602+Lucas-Henrique-Lopes-Costa@users.noreply.github.com>
2020-10-11 13:41:07 -0300

    Começando calc.

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -14,19 +14,22 @@ public class Main {
 
         // pagamento mensal = M
 
-        // System.out.print("Valor do empréstimo: ");
-        // Scanner scannerP = new Scanner(System.in);
-        // Byte P = scannerP.nextByte();
 
-        // System.out.print("Qual a taxa de juros ANUAIS: ");
-        // Scanner scannerI = new Scanner(System.in);
-        // float I = scannerI.nextFloat() / 100; // deve-se inserir com ","
+
+        System.out.print("Valor do empréstimo: ");
+        Scanner scannerP = new Scanner(System.in);
+        Byte P = scannerP.nextByte();
+
+        System.out.print("Qual a taxa de juros ANUAIS: ");
+        Scanner scannerI = new Scanner(System.in);
+        float I = scannerI.nextFloat() / 100; // deve-se inserir com ","
 
         System.out.print("Número de anos: ");
         Scanner scannerN = new Scanner(System.in);
         Byte N = scannerN.nextByte();
 
-        
+        float result = P[I + 1];
+
 
 
         // System.out.print("Juros anuais: ");
