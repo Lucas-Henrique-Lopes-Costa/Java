c4cc2d86da149d5b9ab344caa23b5dcbeb813b7f
Lucas Henrique Lopes Costa <60316602+Lucas-Henrique-Lopes-Costa@users.noreply.github.com>
2020-10-11 12:44:29 -0300

    Formatong

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,13 +1,34 @@
 package com.codewithmosh;
 
+import java.text.NumberFormat;
+
 public class Main {
 
     public static void main(String[] args) {
         String traco = "--------------------------------------------------------------------------------------------------------------------------";
         System.out.println(traco);
-        
-        
 
-        System.out.println("Hello World");
+        // Pormatando o número como um valor MONETÁRIO
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        
+        String resultMoneyString = currency.format(156489873.456);
+
+        System.out.println(resultMoneyString);
+
+        System.out.println(traco);
+
+        // Formatando em forma de PORCENTAGEM
+        NumberFormat percent = NumberFormat.getPercentInstance();
+        
+        String resultPercenString = percent.format(0.15);
+
+        System.out.println(resultPercenString);
+
+        System.out.println(traco);
+
+        // FOrmantando com Method Chaining
+        String resultChaining = NumberFormat.getPercentInstance().format(0.1); // method chaining
+
+        System.out.println(resultChaining);
     }
 }
