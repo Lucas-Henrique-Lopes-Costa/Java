0b996884b74b5682955494e69fa012217d126016
Lucas Henrique Lopes Costa <60316602+Lucas-Henrique-Lopes-Costa@users.noreply.github.com>
2020-10-11 14:03:07 -0300

    calc. pronto

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -14,22 +14,32 @@ public class Main {
 
         // pagamento mensal = M
 
-
+        Scanner scanner = new Scanner(System.in); // isso serve para todas as variáveis
 
         System.out.print("Valor do empréstimo: ");
-        Scanner scannerP = new Scanner(System.in);
-        Byte P = scannerP.nextByte();
+        // Scanner scannerP = new Scanner(System.in);
+        int valorEmprestado = scanner.nextInt();
 
         System.out.print("Qual a taxa de juros ANUAIS: ");
-        Scanner scannerI = new Scanner(System.in);
-        float I = scannerI.nextFloat() / 100; // deve-se inserir com ","
+        // Scanner scannerI = new Scanner(System.in);
+        float taxaJuros = scanner.nextFloat() / 100 / 12; // deve-se inserir com ","
+        
+        // O número que vai receber será em porcentagem => dempois tranforma ele em DECIMAL => divide por 12 para ter os juros MENSAIS
 
         System.out.print("Número de anos: ");
-        Scanner scannerN = new Scanner(System.in);
-        Byte N = scannerN.nextByte();
+        // Scanner scannerN = new Scanner(System.in);
+        byte years = scanner.nextByte();
+        int duraçãoMeses = years * 12; // Transforma os anos em MESES
 
-        float result = P[I + 1];
+        double mortgage =  valorEmprestado
+            * ( taxaJuros * Math.pow(1 + taxaJuros, duraçãoMeses))
+            / ( Math.pow(1 + taxaJuros, duraçãoMeses) -1 );
 
+        // M = P [ i(1 + i)^n ] / [ (1 + i)^n – 1]
+
+        
+
+        System.out.println(mortgage);
 
 
         // System.out.print("Juros anuais: ");
