6ad049bb61f3b91ed669075f59f817c29dee7e9f
Lucas <lucass20586@gmail.com>
2021-02-07 21:28:58 -0300

    add

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -1,52 +1,35 @@
 package com.codewithmosh;
 
-import java.text.NumberFormat;
 import java.util.Scanner;
+
+import javax.print.event.PrintEvent;
 
 public class Main {
 
     public static void main(String[] args) {
-        String traco = "#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-# Bem-Vindo a Calculadora de Hipoteca #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#";
+        String traco = "--------------------------------------------------------------------------------------------------------------------------";
         System.out.println(traco);
 
-        Scanner scanner = new Scanner(System.in); // isso serve para todas as variáveis
+        // for (int i = 5; i > 0; i--) // mais simples
+        //     System.out.println(i + " Hello World");
 
-        System.out.print("Valor do empréstimo: ");
-        // Scanner scannerP = new Scanner(System.in);
-        int valorEmprestado = scanner.nextInt();
+        // System.out.println(traco);
 
-        System.out.print("Qual a taxa de juros ANUAIS (%): ");
-        // Scanner scannerI = new Scanner(System.in);
-        float taxaJurosAnuais = scanner.nextFloat(); // deve-se inserir com "," (float)
-        float taxaJuros = taxaJurosAnuais / 100 / 12;
+        // int i = 1;
+        // while (i <= 5) { // saber exatamente quantas vezes repetiu
+        //     System.out.println("Hello World " + i);
+        //     i++;
+        // }
         
-        // O número que vai receber será em porcentagem => depois tranforma ele em DECIMAL => divide por 12 para ter os juros MENSAIS
+        String input = "";
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Número de anos: ");
-        // Scanner scannerN = new Scanner(System.in);
-        byte years = scanner.nextByte();
-        int duraçãoMeses = years * 12; // Transforma os anos em MESES
+        while (!input.equals("quit")) {
+            System.out.print("Imput: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        }
 
-        double mortgage =  valorEmprestado
-            * ( taxaJuros * Math.pow(1 + taxaJuros, duraçãoMeses))
-            / ( Math.pow(1 + taxaJuros, duraçãoMeses) -1 );
-
-        // M = P [ i(1 + i)^n ] / [ (1 + i)^n – 1]
-
-            // valor emprestado = P        
-            // taxa de juros = i
-            // duração em meses = n
-
-            // pagamento mensal = M
-
-        NumberFormat currency = NumberFormat.getCurrencyInstance();
-        
-        String resultMoneyString = currency.format(mortgage);
-
-        String valorEmprestadoBR = currency.format(valorEmprestado);
-
-        System.out.println("Para quitar o empréstimo de " + valorEmprestadoBR + " em " +  duraçãoMeses / 12 + " anos a uma taxa de juros anual de " + taxaJurosAnuais + "%, o seu pagamento mensal seria de " + resultMoneyString);
-
-        // System.out.print("Juros anuais: ");
+        System.out.println();
     }
 }
